c47712d655caec55ba9080abd27276c24321489b
miabarada <mia.barada2005@gmail.com>
2026-02-22 17:42:47 +0100

    add constants

---
+++ Internship-14-Millionaire/src/constants/questions.js
@@ -0,0 +1,221 @@
+export const questions = [
+  {
+    id: 1,
+    question: "Koji je glavni grad Hrvatske?",
+    answers: [
+      { text: "Split", correct: false },
+      { text: "Zagreb", correct: true },
+      { text: "Rijeka", correct: false },
+      { text: "Osijek", correct: false },
+    ],
+  },
+
+  {
+    id: 2,
+    question: "Na kojem fakultetu se nalazi ured DUMP-a?",
+    answers: [
+      { text: "FESB", correct: true },
+      { text: "FER", correct: false },
+      { text: "PMF", correct: false },
+      { text: "EFST", correct: false },
+    ],
+  },
+
+  {
+    id: 3,
+    question: "Koliko je ljudi na Zemllji",
+    answers: [
+      { text: "2 mlr", correct: false },
+      { text: "8 mlr", correct: true },
+      { text: "6 mlr", correct: false },
+      { text: "5 mlr", correct: false },
+    ],
+  },
+
+  {
+    id: 4,
+    question: "Koje izadnje DUMP Daysa se održava 2026. godine?",
+    answers: [
+      { text: "deveto", correct: false },
+      { text: "deseto", correct: true },
+      { text: "osmo", correct: false },
+      { text: "sedmo", correct: false },
+    ],
+  },
+
+  {
+    id: 5,
+    question: "Koja je najveća rijeka koja teče kroz Hrvatsku",
+    answers: [
+      { text: "Dunav", correct: false },
+      { text: "Sava", correct: true },
+      { text: "Drava", correct: false },
+      { text: "Cetina", correct: false },
+    ],
+  },
+
+  {
+    id: 6,
+    question: "Koji je treći najveći grad Hrvatske?",
+    answers: [
+      { text: "Split", correct: false },
+      { text: "Rijeka", correct: true },
+      { text: "Zagreb", correct: false },
+      { text: "Osijek", correct: false },
+    ],
+  },
+
+  {
+    id: 7,
+    question: "Koji je glavni grad Hrvatske?",
+    answers: [
+      { text: "Split", correct: false },
+      { text: "Zagreb", correct: true },
+      { text: "Rijeka", correct: false },
+      { text: "Osijek", correct: false },
+    ],
+  },
+
+  {
+    id: 8,
+    question: "Koliko područja sadrži DUMP Internship?",
+    answers: [
+      { text: "1", correct: false },
+      { text: "4", correct: true },
+      { text: "3", correct: false },
+      { text: "2", correct: false },
+    ],
+  },
+
+  {
+    id: 9,
+    question: "Koje je godine osnovan DUMP",
+    answers: [
+      { text: "2009.", correct: false },
+      { text: "2010.", correct: true },
+      { text: "2011.", correct: false },
+      { text: "2012.", correct: false },
+    ],
+  },
+
+  {
+    id: 10,
+    question: "Tko je osmislio jezik C?",
+    answers: [
+      { text: "Linus Torvalds", correct: false },
+      { text: "Dennis Ritchie", correct: true },
+      { text: "Ken Thompson", correct: false },
+      { text: "Guido van Rossum", correct: false },
+    ],
+  },
+
+  {
+    id: 11,
+    question: "Koja televizijska serija ima najviše Emmy nagrada u jednoj sezoni?",
+    answers: [
+      { text: "Shogun", correct: false },
+      { text: "Game of thrones", correct: true },
+      { text: "Bear", correct: false },
+      { text: "Stranger things", correct: false },
+    ],
+  },
+
+  {
+    id: 12,
+    question: "Koji glumac ima najviše osvojenih Oscar nagrada?",
+    answers: [
+      { text: "Meryl Streep", correct: false },
+      { text: "Katherine Hepburn", correct: true },
+      { text: "Leonardo DiCaprio", correct: false },
+      { text: "Tom Hanks", correct: false },
+    ],
+  },
+
+  {
+    id: 13,
+    question: "Koji je zadnji DUMP-ov održan ciklus predavanja?",
+    answers: [
+      { text: "Figma 101", correct: false },
+      { text: "Python AI ciklus", correct: true },
+      { text: "Meta Ads", correct: false },
+      { text: "Photoshop", correct: false },
+    ],
+  },
+
+  {
+    id: 14,
+    question: "Koliko je najviše Oscara koje je jedan film osvojio dosada?",
+    answers: [
+      { text: "8", correct: false },
+      { text: "11", correct: true },
+      { text: "9", correct: false },
+      { text: "12", correct: false },
+    ],
+  },
+
+  {
+    id: 15,
+    question: "Dovrši stih poznate pjesme benda Queen: Is this real life? Is this just ...?",
+    answers: [
+      { text: "fiction", correct: false },
+      { text: "fantasy", correct: true },
+      { text: "a dream", correct: false },
+      { text: "possible", correct: false },
+    ],
+  },
+
+  {
+    id: 16,
+    question: "Koji glazbenik ima najviše osvojenih Grammy nagrada dosada?",
+    answers: [
+      { text: "Taylor Swift", correct: false },
+      { text: "Beyonce", correct: true },
+      { text: "Adele", correct: false },
+      { text: "Sir Georg Solti", correct: false },
+    ],
+  },
+
+  {
+    id: 17,
+    question: "Koliko su se puta održale zimske olimpijske igre do 2026. godine (uključujući i 20206.)?",
+    answers: [
+      { text: "24", correct: false },
+      { text: "25", correct: true },
+      { text: "26", correct: false },
+      { text: "27", correct: false },
+    ],
+  },
+
+  {
+    id: 18,
+    question: "Koliko disciplina sadrže zimske olimpijske igre 2026. godine?",
+    answers: [
+      { text: "62", correct: false },
+      { text: "116", correct: true },
+      { text: "89", correct: false },
+      { text: "132", correct: false },
+    ],
+  },
+
+  {
+    id: 19,
+    question: "Koji olimpijac ima najviše osvojenih medalja na Olimpijskim igrama?",
+    answers: [
+      { text: "Usain Bolt", correct: false },
+      { text: "Michael Phelps", correct: true },
+      { text: "Matt Biondi", correct: false },
+      { text: "Isabell Werth", correct: false },
+    ],
+  },
+
+  {
+    id: 20,
+    question: "Koliko traje DUMP Internship",
+    answers: [
+      { text: "5 mjeseci", correct: false },
+      { text: "6 mjeseci", correct: true },
+      { text: "7 mjeseci", correct: false },
+      { text: "8 mjeseci", correct: false },
+    ],
+  }
+];
